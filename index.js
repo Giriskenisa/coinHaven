1ee98b5ccbb4b505402e840e22cdc4a8ccf7286f
Giriskenisa <giriskenisa@gmail.com>
2021-08-18 12:04:05 +0300

    task

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const authRoute = require('./routes/auth');
-const postRoute = require('./routes/posts');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
@@ -12,11 +11,8 @@ dotenv.config();
 mongoose.connect(process.env.DB_CONNECT,{useNewUrlParser:true},
 ()=>console.log('connected'));
 
-
-//Middleware
 app
 .use(express.json())
 .use('/api/user',authRoute)
-.use('/api/posts',postRoute);
 
 app.listen(3000, () => console.log('Server up'));
